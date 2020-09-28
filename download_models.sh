1420e00ad49d719b31c14b4d26bac6ecb771d6b8
ramintoosi <ramin.tsi@gmail.com>
2020-09-28 15:30:59 +0330

    Update download_models.sh
    Bad URLs Fixed.

--- download_models.sh
+++ download_models.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
-BASE_URL="http://cs.stanford.edu/people/jcjohns/fast-neural-style/models/"
+BASE_URL="https://cs.stanford.edu/people/jcjohns/fast-neural-style/models/"
 
-mkdir -p models
+mkdir -p models/instance_norm
+cd models/instance_norm
+curl -LO "$BASE_URL/instance_norm/candy.t7"
+curl -LO "$BASE_URL/instance_norm/la_muse.t7"
+curl -LO "$BASE_URL/instance_norm/mosaic.t7"
+curl -LO "$BASE_URL/instance_norm/feathers.t7"
+curl -LO "$BASE_URL/instance_norm/the_scream.t7"
+curl -LO "$BASE_URL/instance_norm/udnie.t7"
 
-cd models
-#Download style model
-curl -O "$BASE_URL/eccv16/the_wave.t7"
-curl -O "$BASE_URL/eccv16/starry_night.t7"
-curl -O "$BASE_URL/eccv16/la_muse.t7"
-curl -O "$BASE_URL/eccv16/composition_vii.t7"
+mkdir -p ../eccv16
+cd ../eccv16
+curl -LO "$BASE_URL/eccv16/the_wave.t7"
+curl -LO "$BASE_URL/eccv16/starry_night.t7"
+curl -LO "$BASE_URL/eccv16/la_muse.t7"
+curl -LO "$BASE_URL/eccv16/composition_vii.t7"
+cd ..
 
 #Download VGG-19
 wget -c https://gist.githubusercontent.com/ksimonyan/3785162f95cd2d5fee77/raw/bb2b4fe0a9bb0669211cf3d0bc949dfdda173e9e/VGG_ILSVRC_19_layers_deploy.prototxt
